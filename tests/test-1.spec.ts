add8d709cb96a22b9e9c22b45d3b2d3e437bd0ef
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-08-07 17:52:55 +0200

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://cloudtesting.contosotraders.com/');
+  await page.getByPlaceholder('Search by product name or search by image').click();
+  await page.getByPlaceholder('Search by product name or search by image').fill('xbox');
+  await page.getByPlaceholder('Search by product name or search by image').press('Enter');
+  await page.getByPlaceholder('Search by product name or search by image').click();
+  await page.getByRole('img', { name: 'Xbox Wireless Controller Mineral Camo Special Edition' }).click();
+  await page.getByRole('button', { name: '+' }).click();
+  await page.getByRole('button', { name: 'Add To Bag' }).click();
+  await expect(page.locator('div').filter({ hasText: /^Xbox Wireless Controller Mineral Camo Special Edition$/ })).toBeVisible();
+  await page.getByLabel('cart').click();
+  await page.getByRole('link', { name: 'Remove' }).click();
+  await expect(page.locator('div').filter({ hasText: /^Xbox Wireless Controller Mineral Camo Special Edition$/ })).not.toBeVisible();
+});
